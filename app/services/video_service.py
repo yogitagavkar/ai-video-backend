7a7778ad0c29a96ed202743b2a577558a8cce2fa
yogitagavkar <gavkaryogita@gmail.com>
2026-05-06 20:27:20 +0530

    fix:upload folder handling

--- app/services/video_service.py
+++ app/services/video_service.py
@@ -4,6 +4,7 @@ import subprocess
 UPLOAD_DIR = "uploads"
 
 def save_video(file):
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
     file_path = os.path.join(UPLOAD_DIR, file.filename)
     
     with open(file_path, "wb") as f:
